715f85975beb0ac235f20610291761d2cbf8e177
Test <soutakushin@163.com>
2024-08-30 16:23:10 +0800

    [ARPG System][20240830]
    创建BaseCharacter，实现基本的走跑跳

---
+++ Source/UE_ARPGSystem/Public/_ARPG/BaseCharacter.h
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
+#include "BaseCharacter.generated.h"
+
+#pragma region ForwardDeclaration
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+class UCustomMovementComponent;
+
+struct FInputActionValue;
+
+#pragma endregion
+
+DECLARE_LOG_CATEGORY_EXTERN(LogBaseCharacter, Log, All);
+
+UCLASS()
+class UE_ARPGSYSTEM_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+#pragma region DefaultComponents
+
+public:
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+#pragma endregion
+
+#pragma region EnhanceInput
+
+public:
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+private:
+
+	/**
+	 * @brief Called for movement input
+	 *
+	 * @param Value Movement input vector
+	 */
+	void Move(const FInputActionValue& Value);
+
+	/**
+	 * @brief Called for looking input
+	 *
+	 * @param Value Movement input vector
+	 */
+	void Look(const FInputActionValue& Value);
+
+#pragma endregion
+
+
+};
